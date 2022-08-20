7aa3657fbb1c34d64670d4dd01832fae1f82189f
Holat <105239299+Holat@users.noreply.github.com>
2022-08-20 14:31:46 +0100

    final

---
+++ app.js
@@ -0,0 +1,10 @@
+const toggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".links");
+
+
+toggle.addEventListener("click" , () =>{
+    nav.classList.toggle("active")
+})
+
+
+
